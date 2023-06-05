4d811a1352d2edf65e28c67ef5b9da6832ec462a
PerttuValtonen <135586722+PerttuValtonen@users.noreply.github.com>
2023-06-05 10:16:13 +0300

    Initial Commit

---
+++ Highcard/HighcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighcardApp.swift
+//  Highcard
+//
+//  Created by Perttu Valtonen on 5.6.2023.
+//
+
+import SwiftUI
+
+@main
+struct HighcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
